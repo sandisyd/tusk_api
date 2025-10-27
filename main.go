36a2fbc6b53f786283dfc79d5809a047aca34f75
Sandi <sandidwi367@gmail.com>
2025-10-28 05:50:56 +0700

    add mod and main function

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Router
+
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		
+		c.JSON(http.StatusOK, "Welcome to my API")
+	})
+
+	router.Run()
+}
